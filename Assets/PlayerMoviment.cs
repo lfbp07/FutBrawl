6802f80852f25de201934014983e3bf7b5e7a01b
Luis Pereira <lfbp@academy.cin.ufpe.br>
2022-03-17 20:22:13 -0300

    meme

---
+++ Assets/PlayerMoviment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float i = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity += new Vector2(i*10, 0.0f);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+        }
+    }
+}
