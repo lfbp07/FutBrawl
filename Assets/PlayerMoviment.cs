b0159901ff28cec298a9217292ffacaf53c06717
Thalisson <thalissonmt@gmail.com>
2022-03-30 21:57:27 -0300

    add ground check

--- Assets/PlayerMoviment.cs
+++ Assets/PlayerMoviment.cs
@@ -6,6 +6,10 @@ public class PlayerMoviment : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 10.0f;
+    public float jumpForce;
+    private bool doubleJump, isJumping, isGround;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
     private Rigidbody2D rb;
 
     void Start()
@@ -17,11 +21,23 @@ public class PlayerMoviment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float i = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        rb.velocity = new Vector2(i*100, rb.velocity.y);
+        HorizontalMoviment();
+
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            if(isGround){
+                Jump();
+            }
         }
+    }
+
+    private void HorizontalMoviment(){
+        float i = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(i*100, rb.velocity.y);
+    }
+
+    void Jump(){
+        rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
     }
 }
